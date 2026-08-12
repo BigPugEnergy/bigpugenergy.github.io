7383968d4d69cb0a7d5b078cfe30a3301a99cd61
BigPugEnergy <bigpugenergy@outlook.com>
2026-08-12 09:41:51 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
+import "./../styles.css";
 
+import {
+  DATABASE
+} from "./data/generated/database.js";
+
+import {
+  initializeUI
+} from "./ui.js";
+
+
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+
+    initializeUI({
+      database: DATABASE
+    });
+
+  }
+);
